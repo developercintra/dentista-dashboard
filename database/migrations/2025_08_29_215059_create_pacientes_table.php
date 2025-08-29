c9348d5ec84b7a7489c4fdf428938509814b18cc
Gabriel Cintra <cintragabrielcintra@gmail.com>
2025-08-29 19:09:38 -0300

    feature: Criando Tabelas  de Agendamento, Servicos e Pacientes

---
+++ database/migrations/2025_08_29_215059_create_pacientes_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pacientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('telefone',20);
+            $table->strring('email')->unique();
+            $table->string('serviços');
+            $table->date('data_perfeita');
+            $table->time('horario_perfeito');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pacientes');
+    }
+};
